914574a2d64f0100c4be254e54c9e1eed4e45c33
unknown <alin3011@gmail.com>
2017-08-21 23:50:18 +0300

    initial projects

---
+++ chat-java/src/main/java/com/app/chat/ChatApp.java
@@ -0,0 +1,11 @@
+package com.app.chat;
+
+import org.springframework.boot.SpringApplication;
+
+public class ChatApp 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(ChatApp.class, args);
+    }
+}
